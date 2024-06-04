4d820268e3c25418b46f081fa9f4d0d3c5f84d3d
Lotta Saarinen <lssaarinen@outlook.com>
2024-06-04 15:18:56 +0300

    tietokanta tarjouspyyntöihin ja alkeellinen käsittelijä pyyntö.php lisätty

--- tarjous.php
+++ tarjous.php
@@ -2,42 +2,27 @@
 include 'yla.php';
 
 ?>
-
 <html><body>
     
        <p>Jätä tarjouspyyntö lasten tai aikuisten elokuvaelämyspaketista</p><br><br>
   
+<form action="pyynto.php" method="POST">
+  <div><label for="nimi">Nimi:</label>
+    <input id="nimi" type="text" name="nimi"></div>
 
-<form action="" method="POST">
-  <div>
-    <label for="nimi">Nimi:</label>
-    <input id="nimi" type="text" name="nimi">
-    
-  </div>
-  <div>
-    <label>Sähköposti:</label>
-    <input type="text" name="email" >
-  </div>
- 
-   
-  <div>
-    <label>Puhelinnumero:</label>
-    <input type="password" name="salasana1">
-    
-  </div>
+  <div><label>Puhelinnumero:</label>
+    <input type="text" name="puhelinnumero"></div>
 
-
-
-
+    <div><label>Sähköposti:</label>
+    <input type="text" name="email" ></div>
     Viesti<br>
-    <textarea rows=5 cols=60 name=mess></textarea>
-        <br><br>
-        <input type=submit value="Lähetä">
+    <textarea rows=5 cols=60 name="viesti"></textarea>
+    <br><br><input type=submit value="Lähetä">
 </form>
-
     </body></html>
 
-
-
 <?php
+
+
 include 'ala.php'; 
+?>
