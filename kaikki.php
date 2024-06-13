3dbfc561c101acb3a7f5e6637fe5868a4694dd9a
Kalle Sepponen <kalle.sepponen@edu.sasky.fi>
2024-06-13 20:16:28 +0300

    kaikki.php muokattu toimimaan ilman tietokantaa

---
+++ kaikki.php
@@ -0,0 +1,254 @@
+<?php
+include 'yla.php';
+?>
+<main>
+    <table>
+        <thead>
+            <tr>
+                <th>Nimi</th>
+                <th>Ohjaaja</th>
+                <th>Genre</th>
+                <th>Vuosi</th>
+                <th>Miespääosa</th>
+                <th>Naispääosa</th>
+                <th>Ikäraja</th>
+                <th>Kesto</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Inception</td>
+                <td>Christopher Nolan</td>
+                <td>Jännitys</td>
+                <td>2010</td>
+                <td>Leonardo Di Caprio</td>
+                <td>Marion Cotillard</td>
+                <td>12</td>
+                <td>148</td>
+            </tr>
+            <tr>
+                    <td>Titanic</td>
+                    <td>James Cameron</td>
+                    <td>Draama</td>
+                    <td>1994</td>
+                    <td>Leonardo Di Caprio</td>
+                    <td>Kate Winslet</td>
+                    <td>12</td>
+                    <td>195</td>
+                </tr>
+                <tr>
+                    <td>Pulp Fiction</td>
+                    <td>Quentin Tarantino</td>
+                    <td>Rikos</td>
+                    <td>1994</td>
+                    <td>John Travolta</td>
+                    <td>Uma Thurman</td>
+                    <td>16</td>
+                    <td>154</td>
+                </tr>
+                <tr>
+                    <td>The Dark Knight</td>
+                    <td>Christopher Nolan</td>
+                    <td>Rikos</td>
+                    <td>2008</td>
+                    <td>Christian Bale</td>
+                    <td>Maggie Gyllenhaal</td>
+                    <td>12</td>
+                    <td>152</td>
+                </tr>
+                <tr>
+                    <td>The Shawshank Redemption</td>
+                    <td>Frank Darabont</td>
+                    <td>Draama</td>
+                    <td>1994</td>
+                    <td>Tim Robbins</td>
+                    <td>Morgan Freeman</td>
+                    <td>16</td>
+                    <td>142</td>
+                </tr>
+                <tr>
+                    <td>The Matrix</td>
+                    <td>The Wachowskis</td>
+                    <td>Toiminta</td>
+                    <td>1999</td>
+                    <td>Keanu Reeves</td>
+                    <td>Carrie-Ann Moss</td>
+                    <td>16</td>
+                    <td>136</td>
+                </tr>
+                <tr>
+                    <td>Forrest Gump</td>
+                    <td>Robert Zemeckis</td>
+                    <td>Draama</td>
+                    <td>1994</td>
+                    <td>Tom Hanks</td>
+                    <td>Robin Wright</td>
+                    <td>12</td>
+                    <td>142</td>
+                </tr>
+                <tr>
+                    <td>Fight Club</td>
+                    <td>David Fincher</td>
+                    <td>Toiminta</td>
+                    <td>1999</td>
+                    <td>Brad Pitt</td>
+                    <td>Helena Bonham Carter</td>
+                    <td>18</td>
+                    <td>139</td>
+                </tr>
+                <tr>
+                    <td>The Silence of the Lambs</td>
+                    <td>Jonathan Demme</td>
+                    <td>Jännitys</td>
+                    <td>1999</td>
+                    <td>Anthony Hopkins</td>
+                    <td>Jodie Foster</td>
+                    <td>16</td>
+                    <td>118</td>
+                </tr>
+                <tr>
+                    <td>The Godfather</td>
+                    <td>Francis Ford Coppola</td>
+                    <td>Draama</td>
+                    <td>1972</td>
+                    <td>Marlon Brando</td>
+                    <td>Diane Keaton</td>
+                    <td>16</td>
+                    <td>175</td>
+                </tr>
+                <tr>
+                    <td>Superbad</td>
+                    <td>Greg Mottola</td>
+                    <td>Komedia</td>
+                    <td>2007</td>
+                    <td>Michael Cera</td>
+                    <td>Jonah Hill</td>
+                    <td>16</td>
+                    <td>113</td>
+                </tr>
+                <tr>
+                    <td>Hulluna Saraan</td>
+                    <td>Peter Chelsom</td>
+                    <td>Draama</td>
+                    <td>2001</td>
+                    <td>John Cusack</td>
+                    <td>Kate Beckinsale</td>
+                    <td>13</td>
+                    <td>95</td>
+                </tr>
+                <tr>
+                    <td>Toy Story</td>
+                    <td>John Lasseter</td>
+                    <td>Lastenelokuva</td>
+                    <td>1995</td>
+                    <td>Tom Hanks (Woody)</td>
+                    <td>Annie Potts (Bo Peep)</td>
+                    <td>0</td>
+                    <td>81</td>
+                </tr>
+                <tr>
+                    <td>Muumipeikko ja pyrstötähti</td>
+                    <td>Maria Lindberg ja Xavier Picard</td>
+                    <td>Lastenelokuva</td>
+                    <td>2010</td>
+                    <td>Risto Kaskilahti (Muumipappa)</td>
+                    <td>Vuokko Hovatta (Muumimamma)</td>
+                    <td>0</td>
+                    <td>75</td>
+                </tr>
+                <tr>
+                    <td>Finding Nemo</td>
+                    <td>Andrew Stanton</td>
+                    <td>Lastenelokuva</td>
+                    <td>2003</td>
+                    <td>Albert Brooks (Marlin)</td>
+                    <td>Ellen DeGeneres (Dory)</td>
+                    <td>0</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td>Shrek</td>
+                    <td>Andrew Adamson ja Vicky Jenson</td>
+                    <td>Lastenelokuva</td>
+                    <td>2001</td>
+                    <td>Mike Myers (Shrek)</td>
+                    <td>Cameron Diaz (Prinsessa Fiona)</td>
+                    <td>13</td>
+                    <td>90</td>
+                </tr>
+                <tr>
+                    <td>Mulan</td>
+                    <td>Tony Bancroft ja Barry Cook</td>
+                    <td>Lastenelokuva</td>
+                    <td>1998</td>
+                    <td>Eddie Murphy (Mushu)</td>
+                    <td>Ming-Na Wen (Mulan)</td>
+                    <td>0</td>
+                    <td>88</td>
+                </tr>
+                <tr>
+                    <td>The Lion King</td>
+                    <td>Roger Allers ja Rob Minkoff</td>
+                    <td>Lastenelokuva</td>
+                    <td>1994</td>
+                    <td>Matthew Broderick (Simba)</td>
+                    <td>Moira Kelly (Nala)</td>
+                    <td>0</td>
+                    <td>88</td>
+                </tr>
+                <tr>
+                    <td>Frozen</td>
+                    <td>Chris Buck ja Jennifer Lee</td>
+                    <td>Lastenelokuva</td>
+                    <td>2013</td>
+                    <td>Jonathan Groff (Kristoff)</td>
+                    <td>Kristen Bell (Anna)</td>
+                    <td>13</td>
+                    <td>102</td>
+                </tr>
+                <tr>
+                    <td>Despicable Me</td>
+                    <td>Pierre Coffin ja Chris Renaud</td>
+                    <td>Lastenelokuva</td>
+                    <td>2010</td>
+                    <td>Steve Carell (Gru)</td>
+                    <td>Julie Andrews (Marlena)</td>
+                    <td>13</td>
+                    <td>95</td>
+                </tr>
+                <tr>
+                    <td>Moana</td>
+                    <td>Ron Clements ja John Musker</td>
+                    <td>Lastenelokuva</td>
+                    <td>2016</td>
+                    <td>Dwayne Johnson (Maui)</td>
+                    <td>Auli'i Cravalho (Moana)</td>
+                    <td>13</td>
+                    <td>107</td>
+                </tr>
+                <tr>
+                    <td>Up</td>
+                    <td>Pete Docter ja Bob Peterson</td>
+                    <td>Lastenelokuva</td>
+                    <td>2009</td>
+                    <td>Edward Asner (Carl Fredricksen)</td>
+                    <td>Jordan Nagai (Russell)</td>
+                    <td>13</td>
+                    <td>96</td>
+                </tr>
+                <tr>
+                    <td>Hercules</td>
+                    <td>Ron Clements ja John Musker</td>
+                    <td>Lastenelokuva</td>
+                    <td>1997</td>
+                    <td>Tate Donovan (Hercules)</td>
+                    <td>Susan Egan (Megara)</td>
+                    <td>0</td>
+                    <td>93</td>
+                </tr>
+        </tbody>
+    </table>
+</main>
+<?php
+include 'ala.php';
+?>
