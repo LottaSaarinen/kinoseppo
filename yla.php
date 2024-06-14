4afd32edb54ada0b51e3d110c21153ec809dd8e5
Kalle Sepponen <kalle.sepponen@edu.sasky.fi>
2024-06-14 17:08:11 +0300

    lasten leffoja lisätty

--- yla.php
+++ yla.php
@@ -1,36 +1,30 @@
-
-
-<html>
-        <head>
-        <meta charset="UTF-8">
-            
-          <title>etusivu</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <body>
-                   <link rel="preconnect" href="https://fonts.googleapis.com">
-       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet"><link rel="stylesheet" href="tyylit.css">
-     </head> <body>
-             
-        
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+  <meta charset="UTF-8">
+  <title>etusivu</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="tyylit.css">
+</head>
+<body> 
 <nav>
  <ul>
- 
     <p> <a href="elamys.php">Elämyspaketit</a></p>
     <p> <a href="teatterit.php">Teatterimme</a></p>    
-     
             <li class="dropdown">
             <p><a href="javascript:void(0)" class="dropbtn">Elokuvat</a></p>
             <div class="dropdown-content">
             <p><a href="kaikki.php">Kaikki elokuvat</a></p>
             <p><a href="tulossa.php">Tulossa olevat elokuvat</a></p>
-             <p><a href="lapset.php">Lasten elokuvat</a></p>
+            <p><a href="lapset.php">Lasten elokuvat</a></p>
         </div>
       </li>
     </ul>
    </nav>
-   <h1><a href="index.php">📹KINOSEPPO</a></h1>  
-            <br><hr><hr><br><br><br>
-            
-         
+   <h1><a href="index.php">📹KINOSEPPO</a></h1><br><hr><hr><br><br><br>
+    
+   
   
