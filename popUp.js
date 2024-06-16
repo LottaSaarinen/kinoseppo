98ce89fba7e26d2f08b24c3beb78d4170b7ae7a3
Kalle Sepponen <kalle.sepponen@edu.sasky.fi>
2024-06-16 14:38:07 +0300

    popuppia säädetty toimimaan toisella tavalla

--- popUp.js
+++ popUp.js
@@ -5,16 +5,15 @@ var btn = document.querySelector(".submit");
 var span = document.getElementsByClassName("close")[0];
 var closeBtn = document.querySelector(".close-btn");
 
-// Näytä modal kun lähetä nappia painetaan
-btn.onclick = function(event) {
-    event.preventDefault(); // Estä lomakkeen lähetys
-    //modal.style.display = "block";
 
-    var inputs = document.querySelectorAll('#contactForm input[required');
+btn.onclick = function(event) {
+    event.preventDefault(); 
+
+    var inputs = document.querySelectorAll('#contactForm input[required]');
 
     var allFilled = true;
-    inputs.forEach(function(input) {
-        if (!input.value) {
+    inputs.forEach(function(alkio) {
+        if (!alkio.value) {
             allFilled = false;
         }
     });
@@ -26,19 +25,14 @@ btn.onclick = function(event) {
 }
 
 
-
-
-// Sulje modal kun X painetaan
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// Sulje modal kun Sulje nappia painetaan
 closeBtn.onclick = function() {
     modal.style.display = "none";
 }
 
-// Sulje modal jos käyttäjä klikkaa ikkunan ulkopuolelle
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
